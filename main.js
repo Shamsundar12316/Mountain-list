136650fa04d9463e733feec8402abcd2d593001a
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:02:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// part 16// question 20
+Object.defineProperty(exports, "__esModule", { value: true });
+let Mountain_list = ["k2", "mountain everst", "himalya", "margalla", "naga parbat"];
+console.log(`\n\list of mountain`);
+for (let i = 0; i < Mountain_list.length; i++) {
+    console.log(`${Mountain_list[i]}`);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+// part 16// question 20
+
+let Mountain_list: string[] = ["k2","mountain everst","himalya","margalla","naga parbat"];
+console.log(`\n\list of mountain`)
+for(let i=0; i<Mountain_list.length; i++){
+    console.log(`${Mountain_list[i]}`);
+}
